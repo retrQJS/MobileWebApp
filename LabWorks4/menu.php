489001864346e6bebb04f6c9ad2a9c77cae5dc36
robPTY <108301046+robPTY@users.noreply.github.com>
2023-03-12 20:42:12 -0500

    Add files via upload

---
+++ LabWorks4/menu.php
@@ -0,0 +1,35 @@
+<div id="menu" class="red">
+    <center>
+        <ul>
+            <li class="menu-item"><a href="#" class="drp">New Arrivals</a>
+                <div class="menu-content">
+                    <a href="#">Modern Books</a><br>
+                    <a href="#">Manga</a><br>
+                    <a href="#">Children's Books</a><br>
+                </div>
+                </li>
+            <li class="menu-item"><a href="#" class="drp">Genres & Themes</a>
+            <div class="menu-content">
+                <a href="#">Science Fiction</a><br>
+                <a href="#">Old Timers</a><br>
+                <a href="#">Romance</a><br>
+            </div>
+            </li>
+            <li class="menu-item"><a href="#" class="drp">Lending System</a>    
+            <div class="menu-content">
+                <a href="lending_books.php">Check Out</a><br>
+                <a href="#">Returning Book</a><br>
+                <a href="#">Complaints</a><br>
+            </div>
+            </li>
+            <li class="menu-item"><a href="#" class="drp">Miscellaneous</a>
+            <div class="menu-content">
+                <a href="#">Stuffed Animals</a><br>
+                <a href="#">Book Markers</a><br>
+                <a href="#">Free Books</a><br>
+                <a href="home.php">Return Home</a><br>
+            </div>
+            </li>
+        </ul>
+    </center>
+</div>
