70536ba67b6af9def546c0cca5de879bc8fde627
robPTY <108301046+robPTY@users.noreply.github.com>
2023-04-02 23:05:45 -0500

    Add files via upload

---
+++ LabWorks5/faculty/faculty-menu.php
@@ -0,0 +1,27 @@
+<div id="menu" class="red">
+    <center>
+        <ul>
+            <li class="menu-item"><a href="register.php" class="drp">Teach New Course</a>
+            <div class="menu-content">
+                <a href="#">See Students</a><br>
+                <a href="#">Add homework</a><br>
+                <a 
+            <li class="menu-item"><a href="#" class="drp">Enter Grades</a>
+            </li>
+            <li class="menu-item"><a href="#" class="drp">Help</a>    
+            <div class="menu-content">
+                <a href="#">HelpDesk Email</a><br>
+                <a href="#">Submit Ticket</a><br>
+                <a href="#">Complaints</a><br>
+            </div>
+            </li>
+            <li class="menu-item"><a href="index.php" class="drp">Homepage</a>    
+            <div class="menu-content">
+                <a href="#">Instagram</a><br>
+                <a href="#">Twitter</a><br>
+                <a href="../login.php">Login Page</a><br>
+            </div>
+            </li>
+        </ul>
+    </center>
+</div>
